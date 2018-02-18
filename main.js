0e7b11037618d48583970864ae1a232cd62ce729
UF onBrand <onbrand@uberflip.com>
2018-02-18 15:30:45 -0500

    creeps are gathing and reutrning to span/controller

--- main.js
+++ main.js
@@ -9,16 +9,22 @@ const workers = Object.values(Game.creeps).filter(creep => {
 });
 
 function canCreateCreep(spawn, creepToCreate) {
-	const num = Object.keys(creeps);
-	const result = spawn.spawnCreep(creepToCreate, `Worker-${num.length + 1}`, {
+	const num =
+		Math.max(
+			...Object.values(creeps).map(key => parseInt(key.name.split('-')[1]))
+		) + 1;
+	const result = spawn.spawnCreep(creepToCreate, `Worker-${num}`, {
 		dryRun: true
 	});
 	return result;
 }
 
 function createCreep(spawn, creepToCreate) {
-	const num = Object.keys(creeps);
-	const result = spawn.spawnCreep(creepToCreate, `Worker-${num.length + 1}`, {
+	const num =
+		Math.max(
+			...Object.values(creeps).map(key => parseInt(key.name.split('-')[1]))
+		) + 1;
+	const result = spawn.spawnCreep(creepToCreate, `Worker-${num}`, {
 		dryRun: false
 	});
 	return result;
@@ -33,17 +39,46 @@ function getTarget(whereWeAre, max) {
 }
 
 mySpawns.forEach(spawn => {
+	console.log(canCreateCreep(spawn, creepTemplates.worker));
 	if (canCreateCreep(spawn, creepTemplates.worker) === 0) {
 		createCreep(spawn, creepTemplates.worker);
 	}
 });
 
+function goGetResources(creep, target) {
+	if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
+		creep.moveTo(target);
+	}
+}
+
+function dropOffResources(creep, target) {
+	if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+		creep.moveTo(target);
+	}
+}
+
+function getAllDropOffs(creep) {
+	const spawns = creep.room.find(FIND_MY_SPAWNS);
+	const controller = creep.room.controller;
+	return [...spawns, controller];
+}
+
 workers.forEach((creep, i) => {
 	const energySources = creep.room.find(FIND_SOURCES);
+	const dropOffs = getAllDropOffs(creep); //returns an array
+	const energyInRoom = energySources.length > 0;
+	const maxCarry = creep.carryCapacity;
+	const currentCarry = Object.values(creep.carry).reduce(
+		(accumulator, resource) => accumulator + resource,
+		0
+	);
 
-	if (energySources.length > 0) {
+	//if the creep has room to carry more energy
+	if (currentCarry < maxCarry && energySources.length > 0) {
 		const target = energySources[getTarget(i, energySources.length - 1)];
-		console.log(target);
-		creep.moveTo(target);
+		goGetResources(creep, target);
+	} else if (dropOffs.length > 0) {
+		const target = dropOffs[getTarget(i, dropOffs.length - 1)];
+		dropOffResources(creep, target);
 	}
 });
